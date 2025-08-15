4d7586c43cd59d4a660ebfdf4943ee1c736b55f6
Luzia30 <luzia.simao08@aluno.ifce.edu.br>
2025-08-15 13:35:47 -0300

    Add files via upload

---
+++ PrimeiroProgramaJava.java
@@ -0,0 +1,17 @@
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+
+//1ªQuestão:
+
+        System.out.println("Meu Segundo Programa Java: modificando 'Hello, Word!'.");
+
+//2ªQuestão:
+
+        System.out.println("Meu Segundo Programa Java:");
+        System.out.println("Fazendo duas linhas de código.");
+
+//3ªQuestão:
+
+        System.out.println("Meu Segundo Programa Java:\nQuebrando as Linhas.");
+    }
+}
